42b5a95a158fd21f954bd9b10ff14224b55cc80c
silverfox0786 <silverfox0786@hotmail.com>
2016-10-21 19:46:43 +0100

    Test Software 1.30 Fix
    Fixed a few Files issues with Pi Test Software 1.30

--- home/pi/Desktop/ROMS/MakeIDfile.py
+++ home/pi/Desktop/ROMS/MakeIDfile.py
@@ -2,7 +2,7 @@ import os
 import zlib
 
 
-directory='/media/pi/KINGSTON/mame/roms/'
+directory='/home/pi/mame/roms/'
 file_paths = []  # List which will store all of the full filepaths.
 
     # Walk the tree.
@@ -25,26 +25,32 @@ while count<l:
         #got valid rom with .BIN
         #print file_paths[count],
         fname=file_paths[count]
-        
-        gameStart=29
-        temp=gameStart+1
-        while fname[temp]!='/':
+        if ((fname[len(fname)-3:len(fname)]!="CFG")and(fname[len(fname)-3:len(fname)]!="DOC")and (fname[len(fname)-3:len(fname)]!="TXT") ):
+           
+            #print fname
+            gameStart=19
+            temp=gameStart+1
+            #print fname[temp]
+            while fname[temp]!='/':
+                temp=temp+1
+            game=fname[gameStart:temp]
             temp=temp+1
-        game=fname[gameStart:temp]
-        temp=temp+1
-        deviceStart=temp
-        #while fname[temp]!='/':
-        #    temp=temp+1
-        Device=fname[deviceStart:len(fname)]
+            deviceStart=temp
+            #while fname[temp]!='/':
+            #    temp=temp+1
+            Device=fname[deviceStart:len(fname)]
         
-        prev=0
-        for x in open(fname,"rb"):
+            prev=0
+            for x in open(fname,"rb"):
                 prev=zlib.crc32(x,prev)
-        crc="%X"%(prev & 0xFFFFFFFF)
+            crc="%X"%(prev & 0xFFFFFFFF)
 
-        crcfile=open('/home/pi/Desktop/ROMS/ID.crc',"a")
-        crcfile.write(crc+game+' - '+Device+'\n')
-        crcfile.close()
-        print game,crc,Device
+            crcfile=open('/home/pi/Desktop/ROMS/ID.crc',"a")
+            crcfile.write(crc+game+' - '+Device+'\n')
+            crcfile.close()
+            print game,crc,Device
+        
                 
-    count=count+1
+        count=count+1
+
+        print"ID.crc file created!"
